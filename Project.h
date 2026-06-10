41d4bdcb5d411b8b9bd07cb7f631161a281c13e0
Molon100 <milnikar@gmail.com>
2026-06-10 17:06:55 +0300

    Added Project class

---
+++ Project.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <memory>
+#include "Stage.h"
+#include "User.h"
+#include "Task.h"
+#include "ProjectStatus.h"
+
+class Project
+{
+	std::string name;
+	std::string description;
+	Date startDate;
+	std::vector<std::unique_ptr<User>> users;
+	std::vector<std::unique_ptr<Task>> tasks;
+	std::vector<std::unique_ptr<Stage>> stages;
+	ProjectStatus status;
+public:
+
+	Project(const std::string& name, const std::string& description, const Date& startDate);
+};
