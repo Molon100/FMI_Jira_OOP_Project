0c7fe10bf87080aa3526dbbdaed0931c273914f8
Molon100 <milnikar@gmail.com>
2026-06-15 04:13:48 +0300

    Implemented Date somewhat

---
+++ Date.cpp
@@ -0,0 +1,35 @@
+#include "Date.h"
+#include <stdexcept>
+
+void Date::setMonth(unsigned month)
+{
+	if (month <= 0 || month > 12)
+	{
+		throw std::out_of_range("Month has to be between 1 and 12!");
+	}
+}
+
+void Date::setDay(unsigned day)
+{
+	if (day <= 0 || day > 31) 
+	{
+		throw std::out_of_range("Invalid day!");
+	}
+
+}
+
+
+void Date::setYear(unsigned year)
+{
+	if (year < 2026)
+	{
+		throw std::out_of_range("Cannot be a previous year!");
+	}
+}
+
+Date::Date(unsigned day, unsigned month, unsigned year)
+{
+	setYear(year);
+	setMonth(month);
+	setDay(day);
+}
--- GeneralCommand.cpp
+++
@@ -1,5 +0,0 @@
-#include "GeneralCommand.h"
-
-GeneralCommand::GeneralCommand(const std::string& name) : Command({Role::General}, name)
-{
-}
